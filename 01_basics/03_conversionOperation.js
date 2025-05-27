7c794d96e57427c4fed2de8e3a6c25de82537e98
BISSAWJIT CHANDRAW <bissawjit18@gmail.com>
2025-05-27 23:32:06 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = "Bissaw";
+
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* When Number converted
+ "33" => 33
+ "33abc" => NaN
+ true => 1
+ false = 0
+*/
+
+
+// let isLoggedIn = 1;  // true
+// let isLoggedIn = "";  
+let isLoggedIn = "Bissaw";  
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/* When boolean converted
+1 => true; 0 => false;
+"" => false;
+"bissaw" => true
+*/
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
