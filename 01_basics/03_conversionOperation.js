3a00a38d763377cd255fed7f86352330b9465180
BISSAWJIT CHANDRAW <bissawjit18@gmail.com>
2025-05-28 09:54:18 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,5 +37,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************** Operation ***************
+
+let value =3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);    // addition
+// console.log(2-2);    // subtraction
+// console.log(2*2);    // multipication
+// console.log(2**3);  // power
+// console.log(2/2);   // division
+// console.log(2%2);   // remainder/modulation
+
+let str1 = "hello";
+let str2 = "bissa";
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
